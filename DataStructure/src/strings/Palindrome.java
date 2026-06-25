53aae705426126b4a94b48a5cf0e35be6a49526d
DonArjun9 <34016480+DonArjun9@users.noreply.github.com>
2026-06-24 20:11:41 -0500

    string palindrome

---
+++ DataStructure/src/strings/Palindrome.java
@@ -0,0 +1,30 @@
+/*
+Is it a palindrome? Let's find out.
+*/
+
+public class Palindrome {
+    
+  public static void main(String[] args) {
+      
+      String str = "racecaar";
+      
+     System.out.println(isPalindrome(str));
+    
+  }
+  
+  static boolean isPalindrome(String str){
+      
+      int start = 0;
+      int end = str.length()-1;
+      
+      
+      while(start<end){
+          if(str.charAt(start) != str.charAt(end)) return false;
+          start++;
+          end--;
+      }
+      
+      return true;
+  }
+      
+}
