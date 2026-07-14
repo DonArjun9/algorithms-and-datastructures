b1f30e39e50c96871e2b1cd09afbdedfb17e6325
arjunsoma <arjunsoma@10.0.0.141>
2026-07-14 10:14:40 -0500

    string reverse

---
+++ DataStructure/src/strings/StringReverse.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+import java.util.Arrays;
+
+public class StringReverse {
+
+	public static void main() {
+
+		String str = "Hello World 123";
+		char[] cArray = str.toCharArray();
+		reverse(cArray);
+		System.out.println(Arrays.toString(cArray));
+
+	}
+
+	static void reverse(char[] arr) {
+		
+		int start = 0;
+		int end = arr.length-1;
+		
+		while(start<=end) {
+			
+			char temp = arr[end];
+			arr[end]=arr[start];
+			arr[start] = temp;
+			
+			start++;
+			end--;
+			
+		}
+		
+
+	}
+
+}
